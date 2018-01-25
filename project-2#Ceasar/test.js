73119a09fb96fff2f89e4fa03d3aa24431502573
Your Name <youremail@domain.com>
2018-01-25 14:03:38 +0300

    ceasr tests

--- project-2#Ceasar/test.js
+++ project-2#Ceasar/test.js
@@ -2,14 +2,13 @@ let {deepStrictEqual: eq} = require("assert");
 let {encrypt, decrypt} = require("./ceasar");
 let similarText = "abcdef";
 
-eq(encrypt(similarText, 1), encrypt(similarText, 1));
+eq(encrypt(similarText, 3), "defghi");
 eq(encrypt(similarText, 2), "cdefgh");
 eq(encrypt("qwerty", 2), "sygtva");
 
-eq(decrypt(similarText, 3), decrypt(similarText, 3));
+eq(decrypt(similarText, 3), "xyzabc");
 eq(decrypt(similarText, 5), "vwxyza");
 eq(decrypt("sygtva", 2), "qwerty");
 
 
 eq(encrypt(similarText, 5), decrypt(similarText, -5));
-
