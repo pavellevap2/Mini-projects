a5e57976b75ef4481df0a1268036ae6846615698
pavel <paveltextfree@gmail.com>
2018-03-07 15:59:31 +0300

    series err

--- project-11#Async/handleSeries.js
+++ project-11#Async/handleSeries.js
@@ -2,23 +2,32 @@
 let rand = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
 function afn1(next) {
-    setTimeout(() => next("a1"), rand(0, 1000));
+    setTimeout(() => next(null, "a1"), rand(0, 1000));
 }
 
 function afn2(next) {
-    setTimeout(() => next("a2"), rand(0, 1000));
+    setTimeout(() => next(null, "a2"), rand(0, 1000));
 }
 
 function afn3(next) {
-    setTimeout(() => next("a3"), rand(0, 1000));
+    setTimeout(() => next(null, "a3"), rand(0, 1000));
 }
 
+let nextFn =(err, result) =>{
+    if (err){
+        return err
+    } else {
+        return result
+    }
+}
 
 function seriesFn(fn1, fn2, fn3) {
-    return fn1(res => res,
-            fn2(res => res,
-                fn3(res => res)
+
+    return fn1((err, result) => nextFn(err, result),
+            fn2((err, result) => nextFn(err, result),
+                fn3((err, result) => nextFn(err, result))
             )
     )
 }
-seriesFn(afn1, afn2, afn3)
+
+seriesFn(afn1, afn2, afn3);
