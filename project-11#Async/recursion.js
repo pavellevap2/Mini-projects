5a3f2e3da6f9976f34da7c8a7cd450fac4c01c8d
pavel <paveltextfree@gmail.com>
2018-03-08 11:03:48 +0300

    rec

--- project-11#Async/recursion.js
+++ project-11#Async/recursion.js
@@ -1,14 +1,17 @@
-function sumTo(a) {
-    if (a > 1){
-      return  a + sumTo(a -1);
-    } else {
-        return 1;
-    }
+function sumArr(arr) {
+  if (arr.length == 0){
+      return 0
+  }else if(arr.length == 1){
+      return arr[0]
+  }else{
+      return arr[0] + sumArr(arr.slice(1));
+  }
+
 }
-sumTo(5); //15
-
 function arrMax(numArr) {
+    numArr = numArr.slice();
 
+    if (!numArr.length) return numArr;
     if (numArr.length == 1) {
         return numArr[0];
     } else {
@@ -18,11 +21,17 @@ function arrMax(numArr) {
 }
 
 function arrMin(numArr){
+    numArr = numArr.slice();
 
+    if (!numArr.length) return numArr;
     if (numArr.length == 1) {
         return numArr[0]
-    }else {
+    } else {
         numArr[0] < numArr[1] ? numArr.splice(1, 1) : numArr.splice(0, 1)
     }
     return arrMin(numArr)
+}
+function reverse(arr) {
+    if (!arr.length) return arr;
+    return reverse(arr.slice(1)).concat(arr[0]);
 }
